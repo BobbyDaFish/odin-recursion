644e11402256b7754f98851523a97959325f0799
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-05-13 10:26:06 -0600

    Combine exit condition for recursive fibonacci

--- odin_recursion.rb
+++ odin_recursion.rb
@@ -2,8 +2,7 @@ require 'pry-byebug'
 
 def fibs(num, iteration = num, fib = [])
   fib << 0 if fib.empty?
-  return fib if num <= 1
-  return fib if iteration.zero?
+  return fib if num <= 1 || iteration.zero?
 
   if fib.length == 1
     fib << 1
