286d898ca4f03dd0acb06267eaa2c062580555e4
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-05-14 15:59:28 -0600

    Create first take on merge_sort
    Currently not working, only returning the lower number of the first set.

--- odin_recursion.rb
+++ odin_recursion.rb
@@ -16,4 +16,25 @@ def fibs_rec(num, iteration = num, fib = [])
   fibs_rec(num, iteration, fib)
 end
 
-p fibs(8)
+def merge_sort(array)
+  left_array = array[0..((array.length / 2).ceil - 1)]
+  right_array = array[((array.length / 2).floor)..]
+  sorted_array = []
+  left_array = merge_sort(left_array) if left_array.length > 1
+  a = 0
+  b = 0
+  if left_array[a].nil?
+    sorted_array << right_array[b..]
+  elsif right_array[b].nil?
+    sorted_array << left_array[a..]
+  elsif left_array[a] <= right_array[b]
+    sorted_array << left_array[a]
+    a += 1
+  else
+    sorted_array << right_array[b]
+    b += 1
+  end
+  sorted_array
+end
+
+p merge_sort([1, 5, 9, 3, 7])
